eb13553ce8abba0efbdedbed06fc99e4ca5ae248
Daniel Bolajoko <danielbolajoko30@gmail.com>
2021-03-11 15:11:37 +0100

    add template

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 # to power url patterns
 from django.urls import path
 #to import the homePageview form our views
-from .views import homePageView, dashboardView, authorPage, postPage
+from .views import homePageView, dashboardView, authorPage, postPage, HomePageView, AboutPage
 #from .views import dashboardView
 #from .views import authorPage
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path("", homePageView),
     path("dashboard", dashboardView),
     path("authors", authorPage),
-    path("authors/posts", postPage)
+    path("authors/posts", postPage),
+    path("homepage", HomePageView.as_view()),
+    path("about", AboutPage.as_view())
 ]
